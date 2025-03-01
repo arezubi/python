5427c32332e29b682b5d7a4aa6c2adc1c0d220a6
Andres Reyes <andresrz@ext.inditex.com>
2025-03-01 20:42:35 +0100

    Hola Mundo

---
+++ hello.py
@@ -0,0 +1,31 @@
+# Hola Mundo
+print("Hola Python")
+
+"""
+Este es un comentario 
+de varias lineas
+"""
+
+# Consultar el tipo de dato
+print(type("Soy un dato string")) # tipo 'str'
+print(type(5)) # tipo 'int'
+print(type(1.5)) # tipo 'float'
+print(type(True)) # tipo 'boolean'
+
+print("Soy Andrés Reyes y tengo 35 años")
+
+hola = "Hola "
+mundo = "Mundo"
+resultado = hola + mundo
+print(resultado)
+
+nombre = "Andrés"
+edad = 35
+print("Soy " + nombre + " y tengo " + str(edad) + " años")
+
+saludo = input("Hola dime cual es tu nombre: ")
+print("Hola " + saludo)
+
+resultado = 5 + 5
+print(resultado)
+print(type(resultado))
